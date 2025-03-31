a29b6097f00cfca6a68fa7d308b324806065e874
alexassadi <alexassadi12323@gmail.com>
2025-03-31 11:26:22 +0100

    Styling hotfix 3.1.3

--- djangotutorial/apiapp/settings.py
+++ djangotutorial/apiapp/settings.py
@@ -122,6 +122,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'apiapp', 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'apiapp', 'static')
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
