62456b334e0ec12fa9e08b619ac673b25c11300d
alexassadi <alexassadi12323@gmail.com>
2025-03-31 10:40:19 +0100

    Styling hotfix 2

--- djangotutorial/apiapp/settings.py
+++ djangotutorial/apiapp/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = rf"{Path(__file__).resolve().parent.parent}\apiapp"
+BASE_DIR = {Path(__file__).resolve().parent.parent}
 print(BASE_DIR)
 
 
@@ -119,11 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/apiapp/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, '/apiapp/static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '/apiapp/staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
