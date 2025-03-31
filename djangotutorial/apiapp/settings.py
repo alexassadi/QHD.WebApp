677a6878d42f7dfe38187b96a0eded0b061de8f1
alexassadi <alexassadi12323@gmail.com>
2025-03-31 10:52:27 +0100

    Styling hotfix 3.1

--- djangotutorial/apiapp/settings.py
+++ djangotutorial/apiapp/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = {Path(__file__).resolve().parent.parent}
+BASE_DIR = Path(__file__).resolve().parent.parent
 print(BASE_DIR)
 
 
