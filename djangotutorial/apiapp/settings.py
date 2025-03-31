3026c96a5edfa4104c67adca68f8e342deb913d7
alexassadi <alexassadi12323@gmail.com>
2025-03-31 11:52:57 +0100

    Styling hotfix 3.1.8

--- djangotutorial/apiapp/settings.py
+++ djangotutorial/apiapp/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'apiapp', 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'djangotutorial', 'static')
+    os.path.join(BASE_DIR, 'apiapp', 'static')
 ]
 
 
