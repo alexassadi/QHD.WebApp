026082243989955d8ab2c9b25f083f213b7aa348
alexassadi <alexassadi12323@gmail.com>
2025-03-31 10:49:05 +0100

    Styling hotfix 3

--- djangotutorial/apiapp/settings.py
+++ djangotutorial/apiapp/settings.py
@@ -119,11 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/apiapp/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/apiapp/static')
-]
-STATIC_ROOT = os.path.join(BASE_DIR, '/apiapp/staticfiles')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'apiapp', 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
