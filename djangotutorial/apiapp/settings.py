f5e7620f1f1c76789a9a69e9555cc8836be592c2
alexassadi <alexassadi12323@gmail.com>
2025-04-01 09:52:38 +0100

    Styling hotfix 3.1.21

--- djangotutorial/apiapp/settings.py
+++ djangotutorial/apiapp/settings.py
@@ -121,13 +121,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'apiapp', 'static', 'apiapp')
+]
 
 
 # Default primary key field type
