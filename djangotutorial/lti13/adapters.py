d5b36741c686fc1222de6b61924fad4a18c143b0
alexassadi <alexassadi12323@gmail.com>
2025-04-17 23:10:39 +0100

    LTI 1.3 Hotfix 0.0.12

--- djangotutorial/lti13/adapters.py
+++ djangotutorial/lti13/adapters.py
@@ -8,14 +8,16 @@ class DjangoRequest(Request):
         return self._request.is_secure()
 
     def get_param(self, name):
-        # This is the method that caused the NotImplementedError
+        # Needed for LTI launch – must support POST and GET
         return self._request.POST.get(name) or self._request.GET.get(name)
 
     def get_cookie(self, name):
         return self._request.COOKIES.get(name)
 
     def get_header(self, name):
-        return self._request.headers.get(name)
+        # Normalizes headers for case and prefix
+        django_headers = self._request.headers
+        return django_headers.get(name) or django_headers.get(f'HTTP_{name.upper().replace("-", "_")}')
 
     def get_http_method(self):
         return self._request.method
@@ -25,3 +27,9 @@ class DjangoRequest(Request):
 
     def get_content_type(self):
         return self._request.content_type
+
+    def get_path(self):
+        return self._request.path
+
+    def get_query_params(self):
+        return self._request.GET
