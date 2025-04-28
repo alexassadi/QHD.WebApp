9f026b85a832903a4f3cb4896a8bb56232f50288
alexassadi <alexassadi12323@gmail.com>
2025-04-28 16:19:55 +0100

    LTI 1.3 Hotfix 0.0.18

--- djangotutorial/lti13/launch.py
+++ djangotutorial/lti13/launch.py
@@ -1,7 +1,9 @@
 from pylti1p3.message_launch import MessageLaunch
 
-
 class PatchedMessageLaunch(MessageLaunch):
     def _get_request_param(self, key):
-        # Override the method causing NotImplementedError
         return self._request.POST.get(key) or self._request.GET.get(key)
+
+    def enable_check_cookies(self):
+        self._enable_check_cookies = True
+        return self
