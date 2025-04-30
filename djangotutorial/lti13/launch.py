d218ef5e9aaf7d1cb8fb4892624f52e65fc54322
alexassadi <alexassadi12323@gmail.com>
2025-04-30 11:15:32 +0100

    LTI 1.3 Hotfix 0.0.24

--- djangotutorial/lti13/launch.py
+++ djangotutorial/lti13/launch.py
@@ -1,10 +1,19 @@
 from pylti1p3.message_launch import MessageLaunch
 from pylti1p3.exception import LtiException
 from urllib.parse import urlencode
+from django.http import HttpResponseRedirect
 import secrets
 
 
 class PatchedMessageLaunch(MessageLaunch):
+    def __init__(self, request, tool_conf, launch_data_storage, cookie_service=None):
+        super().__init__(request, tool_conf, launch_data_storage, cookie_service=cookie_service)
+        self._request = request
+        self._tool_conf = tool_conf
+        self._launch_data_storage = launch_data_storage
+        self._cookie_service = cookie_service
+        self._cookie_prefix = 'lti1p3_'  # Default for storing state/nonce
+
     def _get_request_param(self, key):
         return self._request.POST.get(key) or self._request.GET.get(key)
 
@@ -13,11 +22,8 @@ class PatchedMessageLaunch(MessageLaunch):
         return self
 
     def get_redirect(self):
-        if not hasattr(self, '_cookie_service'):
+        if not self._cookie_service:
             raise AssertionError("Cookie Service must be set")
-
-        # 🔧 Define cookie prefix manually
-        self._cookie_prefix = 'lti1p3_'  # or anything consistent
 
         state = self._generate_state()
         nonce = self._generate_nonce()
@@ -31,13 +37,12 @@ class PatchedMessageLaunch(MessageLaunch):
 
         params = self._build_auth_params(state=state, nonce=nonce)
         return self._redirect(f"{auth_login_url}?{urlencode(params)}")
-    
+
     def _get_auth_login_url(self):
         iss = self._request.get_param('iss')
         if not iss:
             raise LtiException('Missing "iss" parameter')
         return self._tool_conf.get_auth_login_url(iss)
-
 
     def _generate_state(self):
         return secrets.token_urlsafe(32)
@@ -46,7 +51,6 @@ class PatchedMessageLaunch(MessageLaunch):
         return secrets.token_urlsafe(32)
 
     def _redirect(self, url):
-        from django.http import HttpResponseRedirect
         return HttpResponseRedirect(url)
 
     def _build_auth_params(self, state, nonce):
