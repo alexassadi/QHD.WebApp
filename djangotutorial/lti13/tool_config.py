d8bf095676eec475b382fef0b0446d13337ed597
alexassadi <alexassadi12323@gmail.com>
2025-04-30 11:24:39 +0100

    LTI 1.3 Hotfix 0.0.26

--- djangotutorial/lti13/tool_config.py
+++ djangotutorial/lti13/tool_config.py
@@ -1,7 +1,28 @@
-from pylti1p3.tool_config import ToolConfJsonFile
+import json
+import os
 
 
-class PatchedToolConf(ToolConfJsonFile):
+class PatchedToolConf:
+    def __init__(self, config_file_path):
+        self.config_file_path = config_file_path
+        self._config_data = self._load_config()
+
+    def _load_config(self):
+        with open(self.config_file_path, 'r') as f:
+            return json.load(f)
+
     def get_auth_login_url(self, iss):
-        config = self._load_iss_config(iss)
-        return config.get('auth_login_url')
+        return self._config_data.get(iss, {}).get('auth_login_url')
+
+    def get_redirect_uri(self, request):
+        iss = request.get_param('iss')
+        return self._config_data.get(iss, {}).get('tool_redirect_url')
+
+    def get_client_id(self, iss):
+        return self._config_data.get(iss, {}).get('client_id')
+
+    def get_jwk_config(self):
+        return self._config_data.get('jwks_keys', {})
+
+    def get_deployment_ids(self, iss):
+        return self._config_data.get(iss, {}).get('deployment_ids', [])
