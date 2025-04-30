5d55cf763f9da9fd09c69fa66f3ce7a8d4c3606f
alexassadi <alexassadi12323@gmail.com>
2025-04-30 10:59:09 +0100

    LTI 1.3 Hotfix 0.0.19

--- djangotutorial/lti13/launch.py
+++ djangotutorial/lti13/launch.py
@@ -1,4 +1,7 @@
 from pylti1p3.message_launch import MessageLaunch
+from pylti1p3.exception import LtiException
+from urllib.parse import urlencode
+
 
 class PatchedMessageLaunch(MessageLaunch):
     def _get_request_param(self, key):
@@ -7,3 +10,20 @@ class PatchedMessageLaunch(MessageLaunch):
     def enable_check_cookies(self):
         self._enable_check_cookies = True
         return self
+
+    def get_redirect(self):
+        """
+        This method performs the OpenID Connect redirect from the login initiation step.
+        """
+        state = self._generate_state()
+        nonce = self._generate_nonce()
+
+        self._cookie_service.set_cookie(self._cookie_prefix + 'state', state, max_age=300)
+        self._cookie_service.set_cookie(self._cookie_prefix + 'nonce', nonce, max_age=300)
+
+        redirect_url = self._get_auth_login_url()
+        if not redirect_url:
+            raise LtiException("Unable to build login URL: auth_login_url is missing")
+
+        params = self._build_auth_params(state=state, nonce=nonce)
+        return self._redirect(f"{redirect_url}?{urlencode(params)}")
