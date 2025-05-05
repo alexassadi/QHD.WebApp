d97afc4bfacb1589aa17daf9c2328c3713a66f90
alexassadi <alexassadi12323@gmail.com>
2025-05-05 15:26:37 +0100

    LTI 1.3 Hotfix 0.0.41

--- djangotutorial/lti13/storage.py
+++ djangotutorial/lti13/storage.py
@@ -14,5 +14,5 @@ class DjangoSessionLaunchDataStorage(LaunchDataStorage):
         if f"lti_launch_{launch_id}" in self._session:
             del self._session[f"lti_launch_{launch_id}"]
 
-    def check_state_is_valid(self, state):
-        return self._request.session.get("lti1p3_state") == state
+    def check_state_is_valid(self, request, state):
+        return request.session.get("lti1p3_state") == state
