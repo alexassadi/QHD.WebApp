9e9c7a671c9fc83222dbb3abc4393426f275087f
alexassadi <alexassadi12323@gmail.com>
2025-05-08 10:39:31 +0100

    New word audio implementation Hotfix - 1.0.3

--- djangotutorial/apiapp/views.py
+++ djangotutorial/apiapp/views.py
@@ -251,7 +251,7 @@ def save_and_process_audio(request):
 
                 return JsonResponse({
                     'success': True,
-                    'score': score_data,
+                    'score': result_json,
                     'underlined_sentence': underlined_sentence,
                     'audio_url': converted_url
                 })
