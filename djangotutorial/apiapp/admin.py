a55e537b5404e1cdc9544d690ff21a9634face7f
alexassadi <alexassadi12323@gmail.com>
2025-05-27 03:55:44 +0100

    Generate Sentence - Hotfix 1.2.15

--- djangotutorial/apiapp/admin.py
+++ djangotutorial/apiapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Sentence
+from .models import Sentence, PronunciationResult
 
 # Register your models here.
 admin.site.register(Sentence)
+admin.site.register(PronunciationResult)
