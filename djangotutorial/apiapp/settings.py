7340ae648f9dfb3e3fe567689559060cba8d57e4
alexassadi <alexassadi12323@gmail.com>
2025-05-30 14:22:05 +0100

    Styling Hotfix 5.0.8

--- djangotutorial/apiapp/settings.py
+++ djangotutorial/apiapp/settings.py
@@ -78,7 +78,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'your_project.context_processors.global_timestamp'
+                'apiapp.context_processors.global_timestamp'
             ],
         },
     },
