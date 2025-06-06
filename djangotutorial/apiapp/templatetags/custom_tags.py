aeb6ca54371c3dbb2c28ba8a46fcfacfaccffd95
alexassadi <alexassadi12323@gmail.com>
2025-06-06 19:35:36 +0100

    Generate Sentence - Hotfix 2.0.6

--- djangotutorial/apiapp/templatetags/custom_tags.py
+++ djangotutorial/apiapp/templatetags/custom_tags.py
@@ -2,10 +2,7 @@ from django import template
 
 register = template.Library()
 
-@register.filter
-def get_item(obj, key):
-    # If it's a form, access fields like a dictionary
-    try:
-        return obj[key]
-    except (KeyError, TypeError, AttributeError):
-        return None
+@register.simple_tag
+def form_field(form, prefix, index):
+    key = f"{prefix}{index}"
+    return form[key]
