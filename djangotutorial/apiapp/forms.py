4eec5d46f8653995527b990df6d30c8fcedc6b2b
alexassadi <alexassadi12323@gmail.com>
2025-06-08 22:56:18 +0100

    Generate Sentence - Hotfix 2.0.21

--- djangotutorial/apiapp/forms.py
+++ djangotutorial/apiapp/forms.py
@@ -9,7 +9,7 @@ class SentenceGenerationForm(forms.Form):
                 required=False,
                 label=f"{i}",
                 widget=forms.TextInput(attrs={
-                    'style': 'border-radius: 0; width: 400px; height: 30px; border: 2px solid #000;',
+                    'style': 'border-radius: 0; width: 400px; height: 28px; border: 2px solid #000;',
                 })
             )
 
