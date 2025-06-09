cd2f71f3d29ab943735c18afa060e5563f8440e3
alexassadi <alexassadi12323@gmail.com>
2025-06-10 00:54:12 +0100

    Generate Sentence - Hotfix 2.1.4

--- djangotutorial/apiapp/admin.py
+++ djangotutorial/apiapp/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 from .models import Sentence, PronunciationResult, Client, Profile
 
-# Register your models here.
+# Inline admin to include Profile in the User admin
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+
+# Custom UserAdmin that includes Profile inline
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return []
+        return super().get_inline_instances(request, obj)
+
+# Re-register the User model with the new UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+# Existing model admin registrations
 admin.site.register(Sentence)
 admin.site.register(PronunciationResult)
 
@@ -11,5 +33,5 @@ class ClientAdmin(admin.ModelAdmin):
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'client')
+    list_display = ('user', 'client', 'is_admin')
     list_filter = ('client', 'is_admin')
