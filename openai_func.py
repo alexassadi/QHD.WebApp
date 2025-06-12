90d6da6a364304a861132d690ff65b1dbfe16116
alexassadi <alexassadi12323@gmail.com>
2025-06-12 19:39:59 +0100

    Generate Sentence - Hotfix 2.1.24

--- openai_func.py
+++ openai_func.py
@@ -54,6 +54,8 @@ def generate_sentences(vocab_list):
         for i in fix_reply:
             nested_sentences.append(i)
 
+        return nested_sentences
+
 def send_and_get_prompt(payload):
     client = openai.OpenAI(api_key=API_KEY)
 
